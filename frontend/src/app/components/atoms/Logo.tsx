ed21d6c85eeea8d366955fd388ec06a5f35dcec2
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-13 18:47:13 +0000

    Added the Logo atomic component

---
+++ frontend/src/app/components/atoms/Logo.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import emrLogo from "../../../../public/images/EMR_LOGO.svg"
+
+const Logo = () => {
+    return (
+        <div>
+            <Image
+                src={emrLogo}
+                alt="Eastern Musical Regiment Logo"
+                width={48}
+                height={48}
+            />
+        </div>
+    )
+}
+
+export default Logo
