572e7f07e20be2c270afc7a9bdd58a86c8128e51
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-13 18:53:15 +0000

    Added the Button atomic component

---
+++ frontend/src/app/components/atoms/Button.tsx
@@ -0,0 +1,26 @@
+type ButtonProps = {
+    content: string | React.ReactNode;
+    classname: string;
+    ariaLabel: string;
+    type?: "submit" | "button" | "reset";
+    disabled?: boolean;
+    handleClick?: () => void
+}
+
+const Button = ({content, classname, ariaLabel, type = "button", handleClick = () => {}, disabled = false}: 
+    ButtonProps) => 
+{
+    return (
+        <button
+            aria-label={ariaLabel}
+            className={classname}
+            type={type}
+            disabled={disabled}
+            onClick={handleClick}
+        >
+            {content}
+        </button>
+    )
+}
+
+export default Button
