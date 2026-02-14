6ab7967e2ae47bf7c7859635222b10f2f1c5d42e
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-14 16:54:09 +0000

    Added the YoutubeCard component

---
+++ frontend/src/app/components/molecules/YoutubeCard.tsx
@@ -0,0 +1,53 @@
+"use client"
+import Button from "../atoms/Button"
+import { useState } from "react"
+import playIcon from "../../../../public/images/play-icon.svg"
+import Image from "next/image"
+
+const YoutubeCard = ({videoId}: {videoId: string}) => {
+
+    const [play, setPlay] = useState<boolean>(false)
+
+    return (
+        <div className="relative cursor-pointer group rounded-xl overflow-hidden">
+            {!play && (
+                <>
+                    <Image
+                        src={`https://img.youtube.com/vi/${videoId}/hqdefault.jpg`}
+                        alt="thumbnail image"
+                        className="absolute inset-0 w-full h-full object-cover group-hover:scale-110 transition-all duration-1000"
+                    />
+
+                    <div className="absolute inset-0 bg-fg-black/20 group-hover:bg-fg-black/40 transition"></div>
+
+                    <Button
+                        content={
+                            <Image
+                                src={playIcon}
+                                alt="play icon"
+                                width={30}
+                                height={30}
+                                className="w-16 h-16 md:w-20 md:h-20 bg-gold group-hover:scale-110 z-10 rounded-full flex items-center justify-center shadow-lg transition duration-1000"
+                            />
+                        }
+                        ariaLabel="play button"
+                        classname="absolute inset-0 z-20 flex items-center justify-center cursor-pointer"
+                        handleClick={() => setPlay(true)}
+                    />
+                </>
+            )}
+
+            {play && (
+                <iframe 
+                    title="Youtube video"
+                    src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1`}
+                    allowFullScreen
+                    allow="autoplay; encrypted-media; picture-in-picture"
+                    className="absolute inset-0 w-full h-full"
+                />
+            )}
+        </div>
+    )
+}
+
+export default YoutubeCard
