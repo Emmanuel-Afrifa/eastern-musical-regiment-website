51173c4532933f67f96cc668a2b547c47af454b5
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-25 21:25:26 +0000

    Added animations to the bandlists sections

--- frontend/src/components/organisms/BandsListSection.tsx
+++ frontend/src/components/organisms/BandsListSection.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Section from "../atoms/Section"
 import Container from "../atoms/Container"
 import data from "../../../public/data/data.json"
 import BandCard from "../molecules/BandCard"
+import { motion } from "framer-motion"
+import { staggerContainer, scaleUp } from "@/src/lib/variants"
 
 const BandsListSection = () => {
 
@@ -15,28 +18,44 @@ const BandsListSection = () => {
     return (
         <Section classname="px-6 md:px-10 bg-fg-black">
             <Container>
-                <div className="flex flex-col items-center w-full">
+                <motion.div 
+                    className="flex flex-col items-center w-full"
+                    variants={staggerContainer}
+                    initial="hidden"
+                >
                     <div className="flex flex-wrap justify-center gap-5 md:gap-10 mb-16">
                         {stats.map((stat, index) => {
                             return (
-                                <div 
+                                <motion.div 
                                     key={index}
                                     className="flex flex-col items-center"
+                                    variants={scaleUp}
+                                    initial="hidden"
+                                    whileInView="visible"
+                                    viewport={{once: true}}
                                 >
                                     <p className="section-header-text text-gold mb-2">{stat.value}</p>
                                     <p className="section-description-text text-white">{stat.name}</p>
-                                </div>
+                                </motion.div>
                             )
                         })}
                     </div>
                     <div className="w-full flex flex-col gap-20 xl:gap-12">
                         {bandsData.map(band => {
                             return (
-                                <BandCard key={band.id} bandItem={band} />
+                                <motion.div 
+                                    key={band.id}
+                                    variants={scaleUp}
+                                    initial="hidden"
+                                    whileInView="visible"
+                                    viewport={{once: false, amount: 0.5}}
+                                >
+                                    <BandCard  bandItem={band} />
+                                </motion.div>
                             )
                         })}
                     </div>
-                </div>
+                </motion.div>
             </Container>
         </Section>
     )
