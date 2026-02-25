2e1443d879dcf83463f62dac7df34811415eec46
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-25 15:44:57 +0000

    Added scroll animation to the SlideShow section

--- frontend/src/components/organisms/SlideShowSection.tsx
+++ frontend/src/components/organisms/SlideShowSection.tsx
@@ -1,14 +1,28 @@
+"use client"
 import Section from "../atoms/Section"
 import Container from "../atoms/Container"
 import SlideShow from "../atoms/SlideShow"
+import { motion } from "framer-motion"
+import { staggerContainer } from "@/src/lib/variants"
 
 const SlideShowSection = () => {
     return (
         <Section classname="py-0">
             <Container>
-                <div>
-                    <SlideShow/>
-                </div>
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    variants={staggerContainer}
+                    viewport={{once: false, amount: 0.5}}
+                >
+                    <motion.div
+                        initial={{opacity: 0, scale: 0.3}}
+                        whileInView={{opacity: 1, scale: 1}}
+                        transition={{duration: 1, ease: "easeInOut"}}
+                    >
+                        <SlideShow/>
+                    </motion.div>
+                </motion.div>
             </Container>
         </Section>
     )
