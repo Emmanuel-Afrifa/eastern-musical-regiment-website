7d9ca9474cd2d54c9901a12a4fcc9f8e3c7fe74d
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-26 10:12:17 +0000

    Style the Not Found page responsively

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <div className='w-full h-screen flex items-center justify-center bg-bg-black'>
             <div className="flex flex-col items-center">
-                <h2 className=" overflow-hidden whitespace-nowrap tracking-[6px] text-6xl text-fg-black font-montserrat font-bold animate-text-reveal text-shadow-lg text-shadow-gold/60">
+                <h2 className=" overflow-hidden whitespace-nowrap tracking-[6px] text-5xl md:text-6xl text-fg-black font-montserrat font-bold animate-text-reveal text-shadow-lg text-shadow-gold/60">
                     EMR
                 </h2>
                 <div
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link"
 export default function NotFound() {
     return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <h1>Page nott Found</h1>
+        <div className="w-full h-screen flex justify-center items-center px-10">
+            <div className="flex flex-col gap-6">
+                <h1 className="overflow-hidden whitespace-nowrap text-5xl md:text-6xl text-fg-black animate-text-reveal">404</h1>
+                <p className="text-fg-dark-gray">Sorry, the page you requested is unavailable</p>
+                <Link 
+                    href="/"
+                    className="font-inter text-gold"
+                >
+                    <span className="bg-grow z-0 hover:px-5 hover:text-fg-black inline-block duration-500">Let&apos;s take you home</span>
+                </Link>
+            </div>
         </div>
     )
 }
