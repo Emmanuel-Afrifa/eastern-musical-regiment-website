93078a6d2bc8fbdde384985a53343bedb9b99afd
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-26 18:18:25 +0000

    Added a link to the logo that navigates to the home page

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -1,11 +1,14 @@
 import Logo from "../atoms/Logo"
 import HeaderNavLinks from "../molecules/HeaderNavLinks"
+import Link from "next/link"
 
 const Header = () => {
     return (
         <header className="w-full bg-fg-black lg:bg-transparent lg:backdrop-blur-2xl py-3 flex items-center justify-center fixed z-40">
             <div className="w-full max-w-360 mx-auto flex justify-between items-center px-5 md:px-10">
-                <Logo />
+                <Link  href="/">
+                    <Logo />
+                </Link>
                 <HeaderNavLinks />
             </div>
         </header>
