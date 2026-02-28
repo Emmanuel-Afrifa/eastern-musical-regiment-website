003fdfc6ff941c42c3e3924dcfcdec246844ff2e
Emmanuel-Afrifa <emmaquame9@gmail.com>
2026-02-28 18:48:41 +0000

    Added the milestone underline in the fade animations

--- frontend/src/components/molecules/TimelineItem.tsx
+++ frontend/src/components/molecules/TimelineItem.tsx
@@ -32,25 +32,26 @@ const TimelineItem = ({timelineDetails, index}: TimelineProps) => {
                     <div className={`hidden md:flex justify-center ${isLeft ? "md:translate-x-1/2" : "md:-translate-x-1/2"}`}>
                         <div className="w-4 h-4 rounded-full bg-gold z-5"></div>
                     </div>
-                    <motion.div 
-                        className={`${isLeft ? "md:mr-5 lg:mr-10" : "md:ml-5 lg:ml-10"}`}
+                    <motion.div
                         variants={isLeft ? fadeLeft : fadeRight}
                         transition={{duration: 1}}
                     >
-                        <p className="text-base lg:text-lg">
-                            <span className="inline-block px-6 py-1 rounded-full bg-gold text-fg-black font-semibold">
-                                {timelineDetails.year}
-                            </span>
-                        </p>
-                        <h3 className="font-semibold font-inter text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl my-4 md:my-6">
-                            {timelineDetails.title}
-                        </h3>
-                        <p className="text-fg-gray text-sm md:text-base lg:text-lg mb-6 md:mb-0">
-                            {timelineDetails.description}
-                        </p>
-                    </motion.div>
+                        <div className={`${isLeft ? "md:mr-5 lg:mr-10" : "md:ml-5 lg:ml-10"}`}>
+                            <p className="text-base lg:text-lg">
+                                <span className="inline-block px-6 py-1 rounded-full bg-gold text-fg-black font-semibold">
+                                    {timelineDetails.year}
+                                </span>
+                            </p>
+                            <h3 className="font-semibold font-inter text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl my-4 md:my-6">
+                                {timelineDetails.title}
+                            </h3>
+                            <p className="text-fg-gray text-sm md:text-base lg:text-lg mb-6 md:mb-0">
+                                {timelineDetails.description}
+                            </p>
+                        </div>
 
-                    <div className="md:hidden block w-full h-0.5 bg-fg-gray/20"></div>
+                        <div className="md:hidden block w-full h-0.5 bg-fg-gray/20"></div>
+                    </motion.div>
                 </motion.div>
             </div>
         </div>
